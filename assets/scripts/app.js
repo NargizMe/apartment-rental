2c3eeca4d59ca6340b7af632b17bfdf7c88f4a3b
NargizMe <59040880+NargizMe@users.noreply.github.com>
2022-08-23 00:14:53 +0400

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -41,3 +41,127 @@ function toggleSubscribe(status) {
     overlay.style.display = "block";
   }
 }
+
+async function subscribeToSite() {
+  let subscribeLabel = document.querySelector(".subscribe-label");
+  let email = document.querySelector("#subscribe-email");
+
+  const payload = {
+    email: email.value,
+  };
+
+  try {
+    const response = await fetch("/subscribe", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (Object.keys(data.errorObject).length) {
+      if (data.errorObject.email) {
+        subscribeLabel.textContent = data.errorObject.email;
+      } else {
+        subscribeLabel.textContent = "";
+        toggleSubscribe(false);
+      }
+    } else {
+      subscribeLabel.textContent = "";
+    }
+  } catch (err) {
+    console.log("err", err);
+  }
+}
+
+//sideBar form
+async function sideBarForm() {
+  let checkIn = document.querySelector("#checkIn");
+  let checkOut = document.querySelector("#checkOut");
+  let checkInLabel = document.querySelector(".checkIn-label");
+  let checkOutLabel = document.querySelector(".checkOut-label");
+
+  const payload = {
+    checkIn: checkIn.value,
+    checkOut: checkOut.value,
+  };
+
+  try {
+    const response = await fetch("/search-apartments", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (Object.keys(data.errorObject).length) {
+      if (data.errorObject.checkIn) {
+        checkInLabel.textContent = data.errorObject.checkIn;
+      } else {
+        checkInLabel.textContent = "";
+      }
+      if (data.errorObject.checkOut) {
+        console.log("if");
+        checkOutLabel.textContent = data.errorObject.checkOut;
+      } else {
+        console.log("else");
+        checkOutLabel.textContent = "";
+      }
+    } else {
+      checkInLabel.textContent = "";
+      checkOutLabel.textContent = "";
+    }
+  } catch (err) {
+    console.log("err", err);
+  }
+}
+
+//contact form
+async function contactForm() {
+  let contactMessageLabel = document.querySelector(".contact-message-label");
+  let contactEmailLabel = document.querySelector(".contact-email-label");
+  let name = document.querySelector("#contact-name");
+  let email = document.querySelector("#contact-email");
+  let message = document.querySelector("#contact-message");
+
+  const payload = {
+    name: name.value,
+    email: email.value,
+    message: message.value,
+  };
+
+  try {
+    const response = await fetch("/send-message", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (Object.keys(data.errorObject).length) {
+      if (data.errorObject.email) {
+        contactEmailLabel.textContent = data.errorObject.email;
+      } else {
+        contactEmailLabel.textContent = "";
+      }
+      if (data.errorObject.message) {
+        contactMessageLabel.textContent = data.errorObject.message;
+      } else {
+        contactMessageLabel.textContent = "";
+      }
+    } else {
+      contactMessageLabel.textContent = "";
+      contactEmailLabel.textContent = "";
+    }
+  } catch (err) {
+    console.log("err", err);
+  }
+}
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/search-apartments", (req, res) => {
+  const data = req.body;
+  let errorObject = {};
+  if (!data.checkIn) {
+    errorObject["checkIn"] = "Cannot be empty";
+  }
+  if (!data.checkOut) {
+    errorObject["checkOut"] = "Cannot be empty";
+  }
+  return res.status(418).json({ errorObject });
+});
+
+app.post("/send-message", (req, res) => {
+  const data = req.body;
+  let errorObject = {};
+  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)) {
+    errorObject["email"] = "Email is not correct";
+  }
+  if (!data.email) {
+    errorObject["email"] = "Cannot be empty";
+  }
+  if (!data.message) {
+    errorObject["message"] = "Cannot be empty";
+  }
+  return res.status(418).json({ errorObject });
+});
+
+app.post("/subscribe", (req, res) => {
+  const data = req.body;
+  let errorObject = {};
+  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)) {
+    errorObject["email"] = "Email is not correct";
+  }
+  if (!data.email) {
+    errorObject["email"] = "Cannot be empty";
+  }
+  return res.status(418).json({ errorObject });
+});
+
+app.listen(3000);
